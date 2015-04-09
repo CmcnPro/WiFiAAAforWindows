b3ad56962db7dd25ab60667d4dca2283abd849d3
CmcnPro <zhuoronaldo1994@gmail.com>
2015-04-09 22:44:18 +0800

    2015/4/9

--- WiFiAAA/Form1.Designer.cs
+++ WiFiAAA/Form1.Designer.cs
@@ -184,7 +184,7 @@
             this.AcceptButton = this.LoginBut;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 279);
+            this.ClientSize = new System.Drawing.Size(474, 279);
             this.Controls.Add(this.TokenBut);
             this.Controls.Add(this.labMsg);
             this.Controls.Add(this.labUserName);
@@ -204,7 +204,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "TokenPicture";
-            this.Text = "WiFiAAA v0.9.7.2 RC by 牛索伏特丶無雙";
+            this.Text = "WiFiAAA by CmcnPro";
             this.Load += new System.EventHandler(this.TokenPicture_Load);
             ((System.ComponentModel.ISupportInitialize)(this.TokenPic)).EndInit();
             this.ResumeLayout(false);
